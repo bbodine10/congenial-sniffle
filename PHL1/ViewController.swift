0d484e3c3a2948476ca774e8b09c22b92a98d227
Benjamin Bodine <bbodine10@gmail.com>
2017-03-05 17:17:44 -0500

    oil

---
+++ PHL1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PHL1
+//
+//  Created by Benjamin Bodine on 3/5/17.
+//  Copyright © 2017 Benjamin Bodine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
